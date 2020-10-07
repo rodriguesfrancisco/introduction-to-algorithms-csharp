451fcd1b658523f99004fc5f02a7101e6201fee1
Francisco Rodrigues <f.co.victor@hotmail.com>
2020-10-07 10:48:41 -0300

    Add Insertion Sort algorithm

---
+++ InsertionSort/InsertionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace InsertionSort
+{
+	class InsertionSort
+	{
+		static void Main(string[] args)
+		{
+			int[] array = new int[] { 5, 2, 4, 6, 1, 3, 12, 123, 65, 7, 24 };
+
+			for (var j = 1; j < array.Length; j++)
+			{
+				int key = array[j];
+				int i = j - 1;
+				while (i >= 0 && array[i] > key)
+				{
+					array[i + 1] = array[i];
+					i = i - 1;
+				}
+				array[i + 1] = key;
+			}
+
+			foreach (var item in array)
+			{
+				Console.Write(item + ",");
+			}
+
+			Console.WriteLine();
+		}
+	}
+}
