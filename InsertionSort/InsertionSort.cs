404685e8ab574fe58269009298ff56b954e6f095
Francisco Rodrigues <f.co.victor@hotmail.com>
2020-10-20 11:43:00 -0300

    Exercise 2.1-2

--- InsertionSort/InsertionSort.cs
+++ InsertionSort/InsertionSort.cs
@@ -6,8 +6,28 @@ namespace InsertionSort
 	{
 		static void Main(string[] args)
 		{
-			int[] array = new int[] { 5, 2, 4, 6, 1, 3, 12, 123, 65, 7, 24 };
+			int[] unsortedArray = new int[] { 5, 2, 4, 6, 1, 3, 12, 123, 65, 7, 24 };
+			
+			var nonDecreasingSortedArray = NonDecreasingInsertionSort(unsortedArray);
 
+			Console.WriteLine("Printing non decreasing sorted array");
+			foreach (var item in nonDecreasingSortedArray)
+			{
+				Console.Write(item + ",");
+			}
+			Console.WriteLine();
+
+			var nonIncreasingSortedArray = NonIncreasingInsertionSort(unsortedArray);
+			Console.WriteLine("Printing non increasing sorted array");
+			foreach (var item in nonIncreasingSortedArray)
+			{
+				Console.Write(item + ",");
+			}
+			Console.WriteLine();
+		}
+
+		static int[] NonDecreasingInsertionSort(int[] array)
+		{
 			for (var j = 1; j < array.Length; j++)
 			{
 				int key = array[j];
@@ -19,13 +39,26 @@ namespace InsertionSort
 				}
 				array[i + 1] = key;
 			}
+			return array;
+		}
 
-			foreach (var item in array)
+		// Exercise 2.1-2
+		// Rewrite the INSERTION-SORT procedure to sort into nonincreasing 
+		// instead of nondecreasing order
+		static int[] NonIncreasingInsertionSort(int[] array)
+		{
+			for (var j = 1; j < array.Length; j++)
 			{
-				Console.Write(item + ",");
+				int key = array[j];
+				int i = j - 1;
+				while (i >= 0 && array[i] < key)
+				{
+					array[i + 1] = array[i];
+					i = i - 1;
+				}
+				array[i + 1] = key;
 			}
-
-			Console.WriteLine();
+			return array;
 		}
 	}
 }
